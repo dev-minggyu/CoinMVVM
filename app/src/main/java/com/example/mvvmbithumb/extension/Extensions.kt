c87198aff0b98fe488e95f780129451058731777
SMK <smk940513@gmail.com>
2021-10-15 13:33:57 +0900

    getViewModelFactory Extension 코드 정리

--- app/src/main/java/com/example/mvvmbithumb/extension/Extensions.kt
+++ app/src/main/java/com/example/mvvmbithumb/extension/Extensions.kt
@@ -1,7 +1,6 @@
 package com.example.mvvmbithumb.extension
 
 import android.app.Activity
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -11,14 +10,13 @@ import com.example.mvvmbithumb.viewmodel.ViewModelFactory
 fun <T> MutableLiveData<T>.asLiveData() = this as LiveData<T>
 
 fun Activity.getViewModelFactory(): ViewModelFactory {
-    return getViewModelFactory(applicationContext)
+    val repository = (applicationContext as App).bithumbRepository
+    return ViewModelFactory(repository)
 }
 
 fun Fragment.getViewModelFactory(): ViewModelFactory {
-    return getViewModelFactory(requireContext().applicationContext)
+    val repository = (requireContext().applicationContext as App).bithumbRepository
+    return ViewModelFactory(repository)
 }
 
-fun getViewModelFactory(applicationContext: Context): ViewModelFactory {
-    val repository = (applicationContext as App).bithumbRepository
-    return ViewModelFactory(repository)
 }
