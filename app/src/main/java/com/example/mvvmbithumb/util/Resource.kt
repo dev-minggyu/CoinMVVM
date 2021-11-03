5d0e08e9b3534a4d6fc2f11cb4c06510849f45a8
SMK <smk940513@gmail.com>
2021-11-03 17:37:26 +0900

    Resource Wrapper Class 수정

--- app/src/main/java/com/example/mvvmbithumb/util/Resource.kt
+++ app/src/main/java/com/example/mvvmbithumb/util/Resource.kt
@@ -3,7 +3,7 @@ package com.example.mvvmbithumb.util
 sealed class Resource<T> {
     class Success<T>(val data: T) : Resource<T>()
     class Error<T>(val message: String) : Resource<T>()
-    class Loading<T> : Resource<T>()
+    object Loading : Resource<Nothing>()
 }
 
 //sealed class Resource<T>(
