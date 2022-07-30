bdbd4d6c2fbf95d25e23507377e727c0e5bb0929
SMK <smk940513@gmail.com>
2022-07-30 15:58:51 +0900

    NetworkStateLiveData onLost 처리 방식 개선

--- app/src/main/java/com/example/coinmvvm/util/NetworkStateLiveData.kt
+++ app/src/main/java/com/example/coinmvvm/util/NetworkStateLiveData.kt
@@ -6,26 +6,30 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import android.os.Handler
-import android.os.Looper
 import androidx.lifecycle.LiveData
 import com.example.coinmvvm.constant.enums.NetworkState
+import kotlinx.coroutines.*
 
 class NetworkStateLiveData(context: Context) : LiveData<NetworkState>() {
     private var connectivityManager =
         context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
 
+    private var _job: Job? = null
+    private var _scope = CoroutineScope(Dispatchers.Default)
+
     private val connectivityManagerCallback = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
             super.onAvailable(network)
+            _job?.cancel()
             postValue(NetworkState.CONNECTED)
         }
 
         override fun onLost(network: Network) {
             super.onLost(network)
-            Handler(Looper.getMainLooper()).postDelayed({
+            _job = _scope.launch {
+                delay(1000)
                 updateState()
-            }, 1000)
+            }
         }
     }
 
@@ -53,7 +57,8 @@ class NetworkStateLiveData(context: Context) : LiveData<NetworkState>() {
 
         if (capabilities != null
             && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)) {
+            && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        ) {
             postValue(NetworkState.CONNECTED)
         } else {
             postValue(NetworkState.DISCONNECTED)
