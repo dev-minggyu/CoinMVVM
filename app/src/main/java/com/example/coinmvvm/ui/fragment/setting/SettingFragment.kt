7c934546a20db562bcbafdf92dc5eb2f3d787368
SMK <smk940513@gmail.com>
2022-08-02 18:31:12 +0900

    설정화면 PreferenceFragment 적용 및 기능구현

--- app/src/main/java/com/example/coinmvvm/ui/fragment/setting/SettingFragment.kt
+++ app/src/main/java/com/example/coinmvvm/ui/fragment/setting/SettingFragment.kt
@@ -1,7 +1,27 @@
 package com.example.coinmvvm.ui.fragment.setting
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.example.coinmvvm.R
-import com.example.coinmvvm.databinding.FragmentSettingBinding
-import com.example.coinmvvm.ui.base.BaseFragment
+import com.example.coinmvvm.util.AppThemeManager
 
-class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting)
+class SettingFragment : PreferenceFragmentCompat() {
+
+    private lateinit var _preference: SharedPreferences
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_settings, rootKey)
+
+        _preference = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        _preference.registerOnSharedPreferenceChangeListener { pref, key ->
+            when (key) {
+                "app_theme" -> {
+                    val theme = pref.getString(key, AppThemeManager.THEME_SYSTEM)!!
+                    AppThemeManager.applyTheme(theme)
+                }
+            }
+        }
+    }
+}
