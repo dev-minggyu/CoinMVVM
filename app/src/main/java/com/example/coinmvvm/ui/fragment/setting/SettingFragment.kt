a2d779343843c1d3d69ae5f70cb1de5b2755351c
SMK <smk940513@gmail.com>
2022-08-02 23:45:29 +0900

    [Refactor] SettingFragment 코드 정리

--- app/src/main/java/com/example/coinmvvm/ui/fragment/setting/SettingFragment.kt
+++ app/src/main/java/com/example/coinmvvm/ui/fragment/setting/SettingFragment.kt
@@ -2,25 +2,32 @@ package com.example.coinmvvm.ui.fragment.setting
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
 import com.example.coinmvvm.R
 import com.example.coinmvvm.util.AppThemeManager
 
-class SettingFragment : PreferenceFragmentCompat() {
-
-    private lateinit var _preference: SharedPreferences
-
+class SettingFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_settings, rootKey)
+    }
 
-        _preference = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        _preference.registerOnSharedPreferenceChangeListener { pref, key ->
-            when (key) {
-                getString(R.string.key_pref_app_theme) -> {
-                    val theme = pref.getString(key, AppThemeManager.THEME_SYSTEM)!!
-                    AppThemeManager.applyTheme(theme)
-                }
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(pref: SharedPreferences?, key: String?) {
+        when (key) {
+            getString(R.string.key_pref_app_theme) -> {
+                val theme = pref?.getString(key, AppThemeManager.THEME_SYSTEM)!!
+                AppThemeManager.applyTheme(theme)
             }
         }
     }
