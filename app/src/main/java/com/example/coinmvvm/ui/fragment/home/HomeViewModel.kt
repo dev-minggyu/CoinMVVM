ad97728c587f42d5da5f0a83c3580ee3e028b41e
SMK <smk940513@gmail.com>
2022-08-10 03:46:54 +0900

    [Feat] Ticker 리스트 검색 시 ignoreCase 적용

--- app/src/main/java/com/example/coinmvvm/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/example/coinmvvm/ui/fragment/home/HomeViewModel.kt
@@ -168,7 +168,7 @@ class HomeViewModel @Inject constructor(
 
     private fun filterTicker(): List<Ticker> {
         return if (_filterTickerSymbol.isNotEmpty()) {
-            _tmpTickerList.filter { it.symbol.startsWith(_filterTickerSymbol) }
+            _tmpTickerList.filter { it.symbol.startsWith(_filterTickerSymbol, true) }
         } else {
             _tmpTickerList
         }
